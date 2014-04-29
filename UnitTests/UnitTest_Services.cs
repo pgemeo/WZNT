0a40ebffd30ef6057b70bed2ab32eaf81477ec71
Paulo Santos <pamartins.santos@gmail.com>
2014-04-29 10:53:27 +0100

    commit

---
+++ UnitTests/UnitTest_Services.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest_Services
+    {
+        [TestMethod]
+        public void LOG_START_STARTED_NULL()
+        {
+            Services.Log.LOG_START();            
+        }
+
+        [TestMethod]
+        public void GetCallingMethod_PassGarbage_EmptyString()
+        {
+            Services.Log.Warning("  ");
+        }
+
+        [TestMethod]
+        public void Divide_Numbers_floatExpected()
+        {
+            float c = Services.Log.Divide(5, 5);
+            Assert.AreEqual(c, 5 / 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void Divide_PassZero_ExceptionExpected()
+        {
+            float c = Services.Log.Divide(5, 0);
+            Assert.AreEqual(c, 5 / 5);
+        }
+    }
+}
