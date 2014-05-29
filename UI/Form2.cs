d2174fdcbccec45618b9cf346f362d26848b9c7a
Paulo Santos <pamartins.santos@gmail.com>
2014-05-30 00:25:15 +0100

    Add Form2

---
+++ UI/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Form2 : Form
+    {
+        private ICollection<Services.WZNTServices.GruArtAufEinzelnutzen> mycollection = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            mycollection = Services.DbManager.GetListGruArtAufEinzelnutzen();
+            gruArtAufEinzelnutzenBindingSource.DataSource = mycollection;
+
+            /*
+            foreach (DataGridViewTextBoxColumn cols in gruArtAufEinzelnutzenBindingSource.DataMember)
+            {
+                cols.Visible = false;
+            }
+            */
+
+            dataGridView1.DataSource = gruArtAufEinzelnutzenBindingSource;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ICollection<Services.WZNTServices.GruArtAufEinzelnutzen> list = new List<Services.WZNTServices.GruArtAufEinzelnutzen>();
+            Services.WZNTServices.GruArtAufEinzelnutzen model = new Services.WZNTServices.GruArtAufEinzelnutzen();
+            model.Aufgabe = "Tiago";
+            list.Add(model);
+            gruArtAufEinzelnutzenBindingSource.DataSource = list;
+        }
+    }
+}
