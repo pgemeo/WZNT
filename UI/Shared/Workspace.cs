e955d23ce4c8854843c7176587ca0c1d73433f42
tm2santos <tgemeo@gmail.com>
2014-06-02 18:45:34 +0100

    change

--- UI/Shared/Workspace.cs
+++ UI/Shared/Workspace.cs
@@ -63,46 +63,52 @@ namespace UI.Shared
         public bool SaveElement(object Element, object Data)
         {
             bool ReturnValue = false;
-            if (Type == typeof(GruArtAufEinzelnutzen))
+            if (Element != null)
             {
-                GruArtAufEinzelnutzen Instance = (GruArtAufEinzelnutzen)Element;
-                // Workspace Childs
-                List<GruArtAufEinSprache> WSChilds = (Instance != null && Instance.GruArtAufEinSpraches != null) ?
-                    Instance.GruArtAufEinSpraches.ToList() : new List<GruArtAufEinSprache>();
-                // View Childs
-                List<GruArtAufEinSprache> ViewChilds = (List<GruArtAufEinSprache>)Data;
+                if (Type == typeof(GruArtAufEinzelnutzen))
+                {
+                    GruArtAufEinzelnutzen Instance = (GruArtAufEinzelnutzen)Element;
+                    // Workspace Childs
+                    List<GruArtAufEinSprache> WSChilds = (Instance != null && Instance.GruArtAufEinSpraches != null) ?
+                        Instance.GruArtAufEinSpraches.ToList() : new List<GruArtAufEinSprache>();
+                    // View Childs
+                    List<GruArtAufEinSprache> ViewChilds = (List<GruArtAufEinSprache>)Data;
 
-                // Edit Elements
-                List<GruArtAufEinSprache> EditChilds =
-                    (from VC in ViewChilds
-                     join WSC in WSChilds on VC.Id equals WSC.Id into Join
-                     from J in Join
-                     select VC
-                    ).ToList();
+                    // Edit Elements
+                    List<GruArtAufEinSprache> EditChilds =
+                        (from VC in ViewChilds
+                         join WSC in WSChilds on VC.Id equals WSC.Id into Join
+                         from J in Join
+                         select VC
+                        ).ToList();
 
-                // Delete Elements
-                List<GruArtAufEinSprache> DeleteChilds = WSChilds.Except(ViewChilds).ToList();
-                WSChilds.RemoveAll(X => DeleteChilds.Contains(X));
+                    // Delete Elements
+                    List<GruArtAufEinSprache> DeleteChilds = WSChilds.Except(ViewChilds).ToList();
+                    WSChilds.RemoveAll(X => DeleteChilds.Contains(X));
 
-                // Add Elements
-                List<GruArtAufEinSprache> AddChilds = ViewChilds.Except(WSChilds).ToList();
-                AddChilds = AddChilds.Select(X =>
-                    new GruArtAufEinSprache
-                    {
-                        Id = X.Id,
-                        IdSprache = X.IdSprache,
-                        IdAufgabe = Instance.Id,
-                        Uebersetzung = X.Uebersetzung,
-                        OTimeStamp = X.OTimeStamp
-                    }
-                    ).ToList();
-                WSChilds.AddRange(AddChilds);
+                    // Add Elements
+                    List<GruArtAufEinSprache> AddChilds = ViewChilds.Except(WSChilds).ToList();
+                    AddChilds = AddChilds.Select(X =>
+                        new GruArtAufEinSprache
+                        {
+                            Id = X.Id,
+                            IdSprache = X.IdSprache,
+                            IdAufgabe = Instance.Id,
+                            Uebersetzung = X.Uebersetzung,
+                            OTimeStamp = X.OTimeStamp
+                        }
+                        ).ToList();
+                    WSChilds.AddRange(AddChilds);
 
-                // Remove Empty Elements From View
-                WSChilds.RemoveAll(X => X.Id == 0 && X.IdSprache == 0);
+                    // Remove Empty Elements From View
+                    WSChilds.RemoveAll(X => X.Id == 0 && X.IdSprache == 0);
 
-                // Update Parent
-                Instance.GruArtAufEinSpraches = WSChilds.ToArray();
+                    // Update Parent
+                    Instance.GruArtAufEinSpraches = WSChilds.ToArray();
+
+                    // Return Value
+                    ReturnValue = true;
+                }
             }
             return ReturnValue;
         }
