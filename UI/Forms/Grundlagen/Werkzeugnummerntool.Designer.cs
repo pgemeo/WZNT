66de5d80a9fef4c1f4d1bd7f97e56278f2732677
tm2santos <tgemeo@gmail.com>
2014-06-05 23:21:24 +0100

    change

--- UI/Forms/Grundlagen/Werkzeugnummerntool.Designer.cs
+++ UI/Forms/Grundlagen/Werkzeugnummerntool.Designer.cs
@@ -29,6 +29,7 @@ namespace UI.Modules.Grundlagen
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Werkzeugnummerntool));
             this.flPanelTop = new System.Windows.Forms.FlowLayoutPanel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
@@ -61,45 +62,31 @@ namespace UI.Modules.Grundlagen
             this.flPanelTop.Controls.Add(this.pictureBox3);
             this.flPanelTop.Controls.Add(this.pictureBox5);
             this.flPanelTop.Controls.Add(this.pictureBox6);
-            this.flPanelTop.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.flPanelTop, "flPanelTop");
             this.flPanelTop.Name = "flPanelTop";
-            this.flPanelTop.Size = new System.Drawing.Size(1008, 30);
-            this.flPanelTop.TabIndex = 2;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = global::UI.Properties.Resources.flag_germany;
-            this.pictureBox3.Location = new System.Drawing.Point(10, 3);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(10, 3, 0, 0);
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox5
             // 
             this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox5.Image = global::UI.Properties.Resources.flag_france;
-            this.pictureBox5.Location = new System.Drawing.Point(44, 3);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(10, 3, 0, 0);
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(24, 24);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox6.Image = global::UI.Properties.Resources.flag_russia;
-            this.pictureBox6.Location = new System.Drawing.Point(78, 3);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(10, 3, 0, 0);
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(24, 24);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
             // 
             // panel1
@@ -110,121 +97,82 @@ namespace UI.Modules.Grundlagen
             this.panel1.Controls.Add(this.pictureBox8);
             this.panel1.Controls.Add(this.pictureBox7);
             this.panel1.Controls.Add(this.pictureBox9);
-            this.panel1.Location = new System.Drawing.Point(0, 30);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1008, 50);
-            this.panel1.TabIndex = 5;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = global::UI.Properties.Resources.preferences;
-            this.pictureBox2.Location = new System.Drawing.Point(48, 0);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = global::UI.Properties.Resources.refresh;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox8
             // 
             this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox8.Image = global::UI.Properties.Resources.help;
-            this.pictureBox8.Location = new System.Drawing.Point(858, 0);
-            this.pictureBox8.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.pictureBox8, "pictureBox8");
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox8.TabIndex = 3;
             this.pictureBox8.TabStop = false;
             // 
             // pictureBox7
             // 
             this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox7.Image = global::UI.Properties.Resources.exit;
-            this.pictureBox7.Location = new System.Drawing.Point(954, 0);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox7.TabIndex = 2;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox9
             // 
             this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox9.Image = global::UI.Properties.Resources.undo;
-            this.pictureBox9.Location = new System.Drawing.Point(906, 0);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.pictureBox9, "pictureBox9");
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox9.TabIndex = 4;
             this.pictureBox9.TabStop = false;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel2.Controls.Add(this.lblModTitle);
-            this.panel2.Location = new System.Drawing.Point(50, 100);
-            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.panel2, "panel2");
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(860, 30);
-            this.panel2.TabIndex = 6;
             // 
             // lblModTitle
             // 
-            this.lblModTitle.AutoSize = true;
-            this.lblModTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.lblModTitle, "lblModTitle");
             this.lblModTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblModTitle.Location = new System.Drawing.Point(10, 5);
-            this.lblModTitle.Margin = new System.Windows.Forms.Padding(0);
             this.lblModTitle.Name = "lblModTitle";
-            this.lblModTitle.Size = new System.Drawing.Size(94, 18);
-            this.lblModTitle.TabIndex = 0;
-            this.lblModTitle.Text = "Grundlagen";
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(50, 150);
-            this.treeView1.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(this.treeView1, "treeView1");
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(250, 385);
-            this.treeView1.TabIndex = 0;
             // 
             // flowLayoutContent
             // 
-            this.flowLayoutContent.Location = new System.Drawing.Point(350, 150);
+            resources.ApplyResources(this.flowLayoutContent, "flowLayoutContent");
             this.flowLayoutContent.Name = "flowLayoutContent";
-            this.flowLayoutContent.Size = new System.Drawing.Size(555, 385);
-            this.flowLayoutContent.TabIndex = 7;
             // 
             // Werkzeugnummerntool
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 605);
             this.Controls.Add(this.flowLayoutContent);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.flPanelTop);
             this.Name = "Werkzeugnummerntool";
-            this.Text = "Werkzeugnummerntool";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.flPanelTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
