71639e6ef5ab7fce6634878423ba729a96413e7f
tm2santos <tgemeo@gmail.com>
2014-06-11 23:08:17 +0100

    change

--- UI/Controls/UcGruSysAPiJobl.Designer.cs
+++ UI/Controls/UcGruSysAPiJobl.Designer.cs
@@ -28,18 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UcGruSysAPiJobl));
+            this.btEdit = new System.Windows.Forms.Button();
+            this.flowPanelButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.flowPanelButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btEdit
+            // 
+            resources.ApplyResources(this.btEdit, "btEdit");
+            this.btEdit.Name = "btEdit";
+            this.btEdit.UseVisualStyleBackColor = true;
+            // 
+            // flowPanelButtons
+            // 
+            this.flowPanelButtons.Controls.Add(this.btEdit);
+            this.flowPanelButtons.Controls.Add(this.btSave);
+            this.flowPanelButtons.Controls.Add(this.btCancel);
+            resources.ApplyResources(this.flowPanelButtons, "flowPanelButtons");
+            this.flowPanelButtons.Name = "flowPanelButtons";
+            // 
+            // btSave
+            // 
+            resources.ApplyResources(this.btSave, "btSave");
+            this.btSave.Name = "btSave";
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            resources.ApplyResources(this.btCancel, "btCancel");
+            this.btCancel.Name = "btCancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
             // 
             // UcGruSysAPiJobl
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowPanelButtons);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "UcGruSysAPiJobl";
+            this.flowPanelButtons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelButtons;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
